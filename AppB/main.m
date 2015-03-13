42cd1b7bf63e763048339b9bd1ac7814ebedf48e
github_sych <mac@MacdeMac-mini.local>
2015-03-13 15:06:37 +0800

    添加文字测试

--- AppB/main.m
+++ AppB/main.m
@@ -11,6 +11,7 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        // git test!!!
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
